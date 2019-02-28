e1641087c8fde412e561c47c9b52138f8b14e1d5
Tom Woodward <twwoodward@me.com>
2019-02-28 16:08:28 -0500

    now with entirely different group search

--- index.php
+++ index.php
@@ -37,66 +37,39 @@ function alt_ipe_replacement_learndash_templates( $filepath, $name, $args, $echo
 add_filter('learndash_template','alt_ipe_replacement_learndash_templates', 90, 5);
 
 
-add_action('plugins_loaded', 'alt_ipe_extender_init');
-
-function alt_ipe_extender_init(){
-	if( class_exists('peerFeedback_Queries')) {
-		function alt_ipe_get_group_members(){
-			$user = wp_get_current_user();
-			$user_email = $user->user_email;
-			$group = get_group_users_by_id(2);			
-			return $group;
-		}
+function alt_ipe_get_group_members(){
+	$user_id = get_current_user_id();
+	$user = get_user_meta($user_id,'');	
+	foreach($user as $key=>$value){
+	  if("learndash_group_users_" == substr($key,0,22)){ //such a mess to do partial match
+	   		var_dump($value);
+	   		$users = alt_ipd_get_group_users($value[0]);
+	   		 alt_ipd_users_to_ids($users);
+		  }
+		}		
+			
 	}
+
+
+
+//GET GRP ID FROM LEARN DASH GROUPS
+//GET THE PPL IN THAT GRP
+//GET THE QUIZ ID
+//GET THE SCORES FROM QUIZ
+//RESTRICT BY PPL IN GROUP
+
+function alt_ipd_get_group_users($group_id){
+	$args = array(
+	'meta_key'     => 'learndash_group_users_' . $group_id,
+	'meta_value'   => $group_id,
+	
+ ); 
+	$the_group = get_users( $args );
+	return $the_group;
 }
 
-
-
-
-function get_group_users_by_id($group_id)
-	{
-		global $wpdb;
-		$groupID = $group_id;
-		
-		$sql = "SELECT * FROM " . $wpdb->prefix . DBTABLE_PEER_FEEDBACK_USERS." WHERE groupID=".$groupID;
-	
-		$groupUsers = $wpdb->get_results( $sql );
-		$userCount= $wpdb->num_rows;
-		
-		if($userCount>=1)
-		{
-			$sortedUserArray = array();			
-			foreach($groupUsers as $userInfo)
-			{
-				
-				// Create temp array of users so we can order by surname
-				$firstName = $userInfo->firstName;
-				$lastName = $userInfo->lastName;
-				$email = $userInfo->email;
-				$userID = $userInfo->ID;
-				$password= $userInfo->password;				
-				
-				
-				$sortedUserArray[$userID] = array
-				(
-					'lastName'	=> $lastName,
-					'firstName'	=> $firstName,
-					'email'		=> $email,					
-					'userID'	=> $userID,
-					'password'	=> $password
-				);
-			}
-			
-			// Now sort the array by surname
-			usort($sortedUserArray, function ($a, $b) { return strcmp($a['lastName'], $b['lastName']);});
-			
-			return $sortedUserArray;
-
-		}
-		else
-		{
-			return false;
-		}
-			
+function alt_ipd_users_to_ids($users){
+	foreach ($users as $user) {
+		var_dump($user->ID);
 	}
-	
+}
