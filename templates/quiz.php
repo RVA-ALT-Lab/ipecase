fd06acf7f795edc1dd6e2daffae57bfc10f820ba
Tom Woodward <twwoodward@me.com>
2019-03-18 08:21:54 -0400

    db work

--- templates/quiz.php
+++ templates/quiz.php
@@ -33,11 +33,11 @@
  * 
  * @package LearnDash\Quiz
  */
-//alt_ipd_join_stats_tables_join(alt_ipd_get_stat_refs($quiz_settings['quiz_pro'], alt_ipe_get_group_members()));
-//var_dump($quiz_settings['quiz_pro']);
-//var_dump(alt_ipe_get_group_members());
-//var_dump($quiz_settings['quiz_pro']); //get quiz id as used in wp_wp_pro_quiz_statistic_ref will need user_id & quiz_id to get statistic_ref_id
-var_dump(test_it());
+alt_ipd_join_stats_tables_join(alt_ipd_get_stat_refs($quiz_settings['quiz_pro'], alt_ipe_get_group_members()));
+var_dump($quiz_settings['quiz_pro']);
+var_dump(alt_ipe_get_group_members());
+var_dump($quiz_settings['quiz_pro']); //get quiz id as used in wp_wp_pro_quiz_statistic_ref will need user_id & quiz_id to get statistic_ref_id
+
 echo alt_ipd_get_stat_refs($quiz_settings['quiz_pro'], alt_ipe_get_group_members());
 
 
