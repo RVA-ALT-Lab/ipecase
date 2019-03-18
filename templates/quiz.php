cb9a26c1d96cefcd82a82c0a8f57e91f58f06a7c
Tom Woodward <twwoodward@me.com>
2019-03-18 15:12:27 -0400

    can't var_dump that way

--- templates/quiz.php
+++ templates/quiz.php
@@ -39,7 +39,7 @@ $group_members = alt_ipe_get_group_members();
 $user_ids = implode(', ', $group_members);
 
 
-$data = var_dump(alt_ipd_join_stats_tables_join($user_ids, $quiz_id));
+$data = alt_ipd_join_stats_tables_join($user_ids, $quiz_id);
 doing_math($data);
 
 
