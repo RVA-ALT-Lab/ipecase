e494ded0f7c1334e44b5b148a5f33e92a9ef5f33
Tom Woodward <twwoodward@me.com>
2019-03-19 13:39:07 -0400

    counting it all up

--- index.php
+++ index.php
@@ -107,13 +107,32 @@ function alt_ipd_join_stats_tables_join($user_ids, $quiz_id){
 
 function doing_math($data){
 	$a = [];
+	$previous_value = $data[0]->question_id;
+	$sum = 0;
+	$total = 0;
+	$question_number = 1;
+	echo '<h4>Question ' . $question_number . '</h4>';
 	foreach ($data as $quiz) {
-    	echo  'quiz id: ' .$quiz->quiz_id . ' question_id:' . $quiz->question_id . ' correct_count: ' . $quiz->correct_count .'<br>';
-     $a['question_id'] = $quiz->question_id;
-     $a['question_id'] = $quiz->correct_count;
+     //echo  'quiz id: ' . $quiz->quiz_id . ' question_id:' . $quiz->question_id . ' correct_count: ' . $quiz->correct_count .'<br>';
+     array_push($a, array("question_id"=> $quiz->question_id,"correct_count"=>$quiz->correct_count));
+     $sum += $quiz->correct_count;
+     $total += 1;
+     if ($previous_value != $quiz->question_id){
+     	$question_number += 1;
+     	echo 'correct: ' . $sum . '</br>';
+     	echo 'total: ' . $total . '</br>';
+     	echo 'avg: ' . percent($sum/$total);
+     	echo '<h4>Question ' . $question_number .'</h4>';     	
+     	$sum = 0;
+     	$total = 0;
+     }
+   	 $previous_value = $quiz->question_id;
 	}
-	echo '<h2>unique</h2>';
-	print_r(array_unique($a);
+}
+
+function percent($number){
+	$number = number_format((float)$number, 2, '.', '');
+    return $number * 100 . '%';
 }
 
 // SELECT wp_wp_pro_quiz_statistic_ref.statistic_ref_id, wp_wp_pro_quiz_statistic_ref.quiz_id, wp_wp_pro_quiz_statistic_ref.user_id, wp_wp_pro_quiz_statistic.statistic_ref_id, wp_wp_pro_quiz_statistic.question_id, wp_wp_pro_quiz_statistic.correct_count  
